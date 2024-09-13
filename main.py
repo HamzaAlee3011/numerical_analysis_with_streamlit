226b9322134114a5fdaf2143871ddd1443a67514
HamzaAlee3011 <ameer.hamza.alee3011@gmail.com>
2024-09-13 22:25:13 +0500

    Update main.py
    change the layout of the main page

--- main.py
+++ main.py
@@ -10,26 +10,34 @@ bisection_method_page = st.Page(
     # icon=":material/bid_landscape:",
 )
 
-about_me = st.Page(
-    page="views/about_me.py",
-    title="About Me",
-    icon=":material/account_circle:",
-)
+gsm_method_page = st.Page(
 
-# about_app = st.Page(
-#     page="views/about_app.py",
-#     title="About Application",
-#     icon=":material/info:",
+    page="views/Gauss_Seidal_method.py",
+    title="Gauss Seidel Method",
+    default=True
+    # icon=":material/bid_landscape:",
+)
+# about_me = st.Page(
+#     page="views/about_me.py",
+#     title="About Me",
+#     icon=":material/account_circle:",
 # )
+
+
+about_app = st.Page(
+    page="views/about_app.py",
+    title="About Application",
+    icon=":material/info:",
+)
 
 # NAVIGATION SETUP (WITHOUT SECTIONS)
 # pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
 
 # NAVIGATION SETUP (WITH SECTIONS)
 pg = st.navigation({
-    'Applications': [bisection_method_page],
+    'Applications': [bisection_method_page, gsm_method_page],
     # 'Usage': [instructions],
-    'Info': [about_me]
+    'Info': [about_app]
 })
 
 
